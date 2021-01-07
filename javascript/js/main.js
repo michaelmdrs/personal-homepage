6e028a95d8c3df2d29e35887d557180e50b6c9e8
Michael Santos <mackelldeveloper@gmail.com>
2021-01-07 01:48:29 -0300

    estrutura  básica do javascript

---
+++ javascript/js/main.js
@@ -0,0 +1,27 @@
+
+
+
+/* Lista de dicionário
+
+let frutas = [{nome: 'Melancia', cor: 'Verde'}, {nome: 'Banana', cor: 'Amarela'}]
+
+console.log(frutas[1].cor)
+
+*/
+
+/* Dicionário
+
+let frutas = {nome: 'Melancia', cor: 'Verde'}
+console.log(frutas.nome)
+
+*/
+
+
+/* let compras = ['Banana', 'Leite', 'Amendoim', 'Queijo', 'Tapioca']
+console.log(compras.join(' | ')) */
+
+// compras.push('Pão')
+// compras.pop()
+// console.log(compras.reverse()) inverte a ordem do array
+// console.log(compras.toString()) converte o array para string
+// console.log(compras.join(' | ')) inserir caracteres entre os elementos do array
