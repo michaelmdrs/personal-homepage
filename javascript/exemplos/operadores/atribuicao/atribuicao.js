d82a86c7a53e786ffd5cecf66e066e475af14bb2
Michael <mackelldeveloper@gmail.com>
2021-01-16 16:40:40 -0300

    Básico de atribuição

---
+++ javascript/exemplos/operadores/atribuicao/atribuicao.js
@@ -0,0 +1,18 @@
+// Atribuição
+x = y
+
+// Atribuição de adição
+x = x + y
+x += y
+
+// Atribuição de subtração
+x = x - y
+x -= y
+
+// Atribuição de divisão
+x = x * y
+x *= y
+
+// Atribuição de resto
+x = x % y
+x %= y
