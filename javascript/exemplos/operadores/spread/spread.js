1494972f8e303559b5b6e2c35a9729998525312e
Michael <mackelldeveloper@gmail.com>
2021-01-16 17:11:17 -0300

    Spread básico

---
+++ javascript/exemplos/operadores/spread/spread.js
@@ -0,0 +1,8 @@
+// Spread ...
+var partes = ['Ombro', 'Joelhos']
+var musica = ['Cabeça', ...partes, 'e', 'pé']
+// console.log(musica)
+
+function fn(x, y, z) { }
+var args = [0, 1, 2]
+fn(...args)
